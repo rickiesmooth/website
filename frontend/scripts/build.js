6c4589ffa6075640514d76fdea88ba504f536697
Rick <rick.p.smit@gmail.com>
2019-09-17 11:32:35 +0200

    chore: removed old service discovery logic

--- frontend/scripts/build.js
+++ frontend/scripts/build.js
@@ -1,14 +1,9 @@
 const fs = require("fs");
 const path = require("path");
-const outputs = require("./outputs.json");
 const frontendDir = path.resolve(__dirname, "../frontend");
 const frontendBuildDir = path.resolve(frontendDir, "build");
 const htmlFilePath = path.resolve(frontendDir, "index.html");
 const jsFilePath = path.resolve(frontendDir, "index.js");
-
-if (!outputs) {
-  throw new Error("outputs.json not found");
-}
 
 if (!htmlFilePath) {
   throw new Error("HTML file not found");
@@ -27,30 +22,23 @@ if (!htmlFilePath) {
 })();
 
 function prepareHtml() {
-  const exposedServices = outputs
-    .map(
-      ({ OutputKey, OutputValue }) => `window.${OutputKey} = "${OutputValue}";`
-    )
-    .join("");
   console.log("preparing html...");
   return new Promise((resolve, reject) =>
     Promise.all([
       fs.promises.readFile(htmlFilePath, "utf8"),
       fs.promises.mkdir(frontendBuildDir, { recursive: true })
     ])
-      .then(([html]) =>
-        resolve(html.replace(`("__SERVICES__");`, exposedServices))
-      )
+      .then(([html]) => resolve(html))
       .catch(e => reject(e))
   );
 }
 
-async function moveFilesToBuildDirectory(preparedHtml) {
+async function moveFilesToBuildDirectory(html) {
   const htmlFileBuildPath = path.resolve(frontendBuildDir, "index.html");
   const jsFileBuildPath = path.resolve(frontendBuildDir, "index.js");
   console.log("copying files...");
   await Promise.all([
-    fs.promises.writeFile(htmlFileBuildPath, preparedHtml),
+    fs.promises.writeFile(htmlFileBuildPath, html),
     fs.promises.copyFile(jsFilePath, jsFileBuildPath)
   ]);
 }
