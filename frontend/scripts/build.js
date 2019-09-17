2734db0d2a4a7bcce52f31387b9c62e468516ff4
Rick <rick.p.smit@gmail.com>
2019-09-17 11:43:00 +0200

    chore: only copy files to build directory

--- frontend/scripts/build.js
+++ frontend/scripts/build.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const frontendDir = path.resolve(__dirname, "../frontend");
+const frontendDir = path.resolve(__dirname, "../");
 const frontendBuildDir = path.resolve(frontendDir, "build");
 const htmlFilePath = path.resolve(frontendDir, "index.html");
 const jsFilePath = path.resolve(frontendDir, "index.js");
@@ -12,33 +12,22 @@ if (!htmlFilePath) {
 (async function build() {
   console.log("building...");
   console.time("build:time");
-  await prepareHtml()
-    .then(html => moveFilesToBuildDirectory(html))
-    .catch(e => {
-      throw new Error(e.message);
-    });
+  try {
+    await moveFilesToBuildDirectory();
+  } catch (error) {
+    throw new Error(error);
+  }
   console.timeEnd("build:time");
   console.log("done!");
 })();
 
-function prepareHtml() {
-  console.log("preparing html...");
-  return new Promise((resolve, reject) =>
-    Promise.all([
-      fs.promises.readFile(htmlFilePath, "utf8"),
-      fs.promises.mkdir(frontendBuildDir, { recursive: true })
-    ])
-      .then(([html]) => resolve(html))
-      .catch(e => reject(e))
-  );
-}
-
-async function moveFilesToBuildDirectory(html) {
+async function moveFilesToBuildDirectory() {
   const htmlFileBuildPath = path.resolve(frontendBuildDir, "index.html");
   const jsFileBuildPath = path.resolve(frontendBuildDir, "index.js");
   console.log("copying files...");
+  await fs.promises.mkdir(frontendBuildDir, { recursive: true });
   await Promise.all([
-    fs.promises.writeFile(htmlFileBuildPath, html),
+    fs.promises.copyFile(htmlFilePath, htmlFileBuildPath),
     fs.promises.copyFile(jsFilePath, jsFileBuildPath)
   ]);
 }
