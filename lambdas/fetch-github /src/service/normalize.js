ba87dc72459eea7ba7f4e627b9753aa79e8861c1
Rick Smit <rick.p.smit@gmail.com>
2019-10-11 12:12:39 +0200

    Task/syntax improvements (#16)
    * show pr instead of recent contributions
    
    * dont use dontenv when deploying
    
    * task: small syntax improvement
    
    * v0.5.1-alpha.0

--- lambdas/fetch-github /src/service/normalize.js
+++ lambdas/fetch-github /src/service/normalize.js
@@ -20,18 +20,14 @@ exports.normalizeGithubData = ({ errors, data }) => {
         return { ...rest, topics: edges.map(edge => edge.node.topic.name) };
       }),
       contributions: nodes
-        .filter(pr => {
-          if (
+        .filter(
+          pr =>
             !pr.url.includes("/vigour-io/") &&
             !pr.url.includes("/rickiesmooth/") &&
             !pr.url.includes("/ubertrace/")
-          ) {
-            return true;
-          }
-        })
+        )
         .map(pr => {
-          const parsed = url.parse(pr.url);
-          const [_, owner, repoName] = parsed.pathname.split("/");
+          const [_, owner, repoName] = url.parse(pr.url).pathname.split("/");
           return {
             url: pr.url,
             repo: [owner, repoName].join("/")
